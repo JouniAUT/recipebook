ec47bead66a62ebaed5db096a52bfc8d8d900c44
JouniAUT <jouni.au.tuomela@gmail.com>
2023-11-15 11:39:19 +0200

    Final tests update

--- src/test/java/hh/sof03/recipebook/domain/UserRepositoryTest.java
+++ src/test/java/hh/sof03/recipebook/domain/UserRepositoryTest.java
@@ -1,5 +1,43 @@
 package hh.sof03.recipebook.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class UserRepositoryTest {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+	public void findByUserName() {
+		List<User> users = userRepository.findUserByUsername("user");
+		
+		assertThat(users).hasSize(1);
+		assertThat(users.get(0).getId()).isEqualTo(1);
+	}
+	
+	@Test
+	public void createNewUser() {
+		User user = new User("Jouni", "$2a$10$cm3MY3erVp08U8WjkTHb4e9qJc.onDW2EkrpruPCs5hbxyD1.p0b6", "USER");
+		userRepository.save(user);
+		
+		assertThat(user.getId()).isNotNull();
+	}
+	
+	@Test
+	public void deleteAllUsers() {
+		userRepository.deleteAll();
+		
+		assertThat(userRepository.count()).isEqualTo(0);
+	}
+	
 }
